d35ba235242bcd27bf9d5413119f70396e58c7e6
hashaam <hashaamkhurshid18@gmail.com>
2021-09-24 18:55:51 +0500

    Dashboard Activity Created

--- app/src/main/java/com/example/interestcalculator/Activities/DashboardActivity.java
+++ app/src/main/java/com/example/interestcalculator/Activities/DashboardActivity.java
@@ -1,16 +1,86 @@
 package com.example.interestcalculator.Activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.interestcalculator.Fragments.HistoryFragment;
+import com.example.interestcalculator.Fragments.HomeFragment;
+import com.example.interestcalculator.Fragments.SavedFragment;
+import com.example.interestcalculator.Fragments.SettingsFragment;
 import com.example.interestcalculator.R;
+import com.example.interestcalculator.databinding.ActivityDashboardBinding;
 
 public class DashboardActivity extends AppCompatActivity {
 
+    ActivityDashboardBinding dashboardBinding;
+
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        dashboardBinding = ActivityDashboardBinding.inflate(getLayoutInflater());
+        setContentView(dashboardBinding.getRoot());
+
+        if (savedInstanceState == null) {
+            HomeFragment homeFragment = new HomeFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, homeFragment).commit();
+        }
+        setSupportActionBar(dashboardBinding.DBappbar);
+        setupBottomNavigaion();
+//        dashboardBinding.bottomNavigation.setOnItemSelectedListener(navigationItemSelectedListener);
+
+
     }
+
+    @SuppressLint("NonConstantResourceId")
+    private void setupBottomNavigaion() {
+        dashboardBinding.bottomNavigation.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.history:
+                    selectedFragment = new HistoryFragment();
+                    break;
+                case R.id.saved:
+                    selectedFragment = new SavedFragment();
+                    break;
+                case R.id.settings:
+                    selectedFragment = new SettingsFragment();
+                    break;
+
+            }
+            assert selectedFragment != null;
+            getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, selectedFragment).commit();
+            return true;
+        });
+    }
+//    @SuppressLint("NonConstantResourceId")
+//    private final  navigationItemSelectedListener =
+//            item -> {
+//                Fragment selectedFragment = null;
+//                switch (item.getItemId()) {
+//                    case R.id.home:
+//                        selectedFragment = new HomeFragment();
+//                        break;
+//                    case R.id.history:
+//                        selectedFragment = new HistoryFragment();
+//                        break;
+//                    case R.id.saved:
+//                        selectedFragment = new SavedFragment();
+//                        break;
+//                    case R.id.settings:
+//                        selectedFragment = new SettingsFragment();
+//                        break;
+//
+//                }
+//                assert selectedFragment != null;
+//                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, selectedFragment).commit();
+//                return true;
+//            };
 }
--- app/src/main/java/com/example/interestcalculator/Activities/SplashScreen.java
+++ app/src/main/java/com/example/interestcalculator/Activities/SplashScreen.java
@@ -1,6 +1,8 @@
 package com.example.interestcalculator.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +10,17 @@ import com.example.interestcalculator.R;
 
 public class SplashScreen extends AppCompatActivity {
 
+    public static final int SPLASH_DELAY = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //  ! -> Splash Screen
+        new Handler().postDelayed((Runnable) () -> {
+            startActivity(new Intent(SplashScreen.this, DashboardActivity.class));
+            finish();
+        }, SPLASH_DELAY);
     }
 }
