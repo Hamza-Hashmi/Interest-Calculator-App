802c69e6a1593a36f69e9f63f14037722952444e
Hamza-Hashmi <hamzayh55@gmail.com>
2021-09-26 16:39:28 +0500

    Initial Commit Interest Calculator

---
+++ app/src/main/java/com/example/interestcalculator/Adapter/HistoryAdapter.java
@@ -0,0 +1,68 @@
+package com.example.interestcalculator.Adapter;
+
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.interestcalculator.databinding.CustomHistoryLayoutBinding;
+import com.example.interestcalculator.models.InterestModel;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.HistoryViewHolder> {
+
+    ArrayList<InterestModel> list;
+    public HistoryAdapter(ArrayList<InterestModel> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public HistoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new HistoryViewHolder(CustomHistoryLayoutBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HistoryViewHolder holder, int position) {
+        InterestModel model = list.get(position);
+
+       holder.binding.tvGivendate.setText(model.getGivenDate());
+       holder.binding.tvDuration.setText(model.getDurationPeriod());
+       holder.binding.tvInterest.setText(model.getInterestAmount());
+       holder.binding.tvInterestAmount.setText(model.getInterest());
+       holder.binding.tvInterestType.setText(model.getPrincipalAmount());
+       holder.binding.tvReturnDate.setText(model.getReturnDate());
+       holder.binding.totalAmountTv.setText("Total Amount : " + model.getTotalAmount());
+
+        Calendar cal = Calendar.getInstance(Locale.ENGLISH);
+        cal.setTimeInMillis(Long.parseLong(model.getCurrentDate()));
+        String date = DateFormat.format("dd-MM-yyyy", cal).toString();
+
+        holder.binding.savedDate.setText(date);
+
+        holder.binding.btnDeleteHistory.setOnClickListener(view ->{
+           Log.e("TAG", "onBindViewHolder: delete clicked" );
+       });
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    class HistoryViewHolder extends RecyclerView.ViewHolder{
+
+        CustomHistoryLayoutBinding binding;
+        public HistoryViewHolder(@NonNull CustomHistoryLayoutBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+}
--- app/src/main/java/com/example/interestcalculator/Fragments/HistoryFragment.java
+++ app/src/main/java/com/example/interestcalculator/Fragments/HistoryFragment.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+import com.example.interestcalculator.Adapter.HistoryAdapter;
 import com.example.interestcalculator.DbHeleper;
 import com.example.interestcalculator.databinding.FragmentHiistoryBinding;
 import com.example.interestcalculator.models.InterestModel;
@@ -22,22 +23,22 @@ public class HistoryFragment extends Fragment {
     FragmentHiistoryBinding hiistoryBinding;
     DbHeleper dbHeleper;
     ArrayList<InterestModel> intersetHistoryList;
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        dbHeleper = new DbHeleper(requireContext());
-
-
-        intersetHistoryList = new ArrayList<>(dbHeleper.getHistory());
-        intersetHistoryList.forEach((model) -> Log.e("TAG", "onCreate: " + model.getDurationPeriod() ));
-    }
+    HistoryAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         hiistoryBinding = FragmentHiistoryBinding.inflate(inflater, container, false);
+        dbHeleper = new DbHeleper(requireContext());
+
+
+        intersetHistoryList = new ArrayList<>(dbHeleper.getHistory());
+        //intersetHistoryList.forEach((model) -> Log.e("TAG", "onCreate: " + model.getDurationPeriod() ));
+
+        adapter = new HistoryAdapter(intersetHistoryList);
+        hiistoryBinding.historyRv.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
         return hiistoryBinding.getRoot();
     }
 }
