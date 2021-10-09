a45ed50ddce210f4ac757301a755c436b031fd2c
Hamza-Hashmi <hamzayh55@gmail.com>
2021-10-09 12:05:14 +0500

    "locale set"

--- app/src/main/java/com/example/interestcalculator/Activities/SplashScreen.java
+++ app/src/main/java/com/example/interestcalculator/Activities/SplashScreen.java
@@ -1,5 +1,7 @@
 package com.example.interestcalculator.Activities;
 
+import static com.example.interestcalculator.widgets.Commons.LANGUAGE;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -7,15 +9,35 @@ import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.interestcalculator.R;
+import com.example.interestcalculator.models.LocalHelper;
+import com.example.interestcalculator.widgets.SharedHelper;
 
 public class SplashScreen extends AppCompatActivity {
 
     public static final int SPLASH_DELAY = 2000;
 
+    String language;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        language = SharedHelper.getKey(this,LANGUAGE);
+
+        if (!LANGUAGE.equals("")){
+            if (LANGUAGE.equals("Hindi")){
+                LocalHelper.setLocale(this,"hi");
+
+            }else if (LANGUAGE.equals("Kannada")){
+                LocalHelper.setLocale(this,"kn");
+
+            }else if(LANGUAGE.equals("Malaylam")){
+                LocalHelper.setLocale(this,"ml");
+            }else if (LANGUAGE.equals("Telugu")){
+                LocalHelper.setLocale(this,"te");
+            }
+        }
 
         //  ! -> Splash Screen
         new Handler().postDelayed((Runnable) () -> {
